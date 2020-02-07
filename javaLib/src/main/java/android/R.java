18b6c406489858dd70881cddc7507f708579fcc5
jjsoa1 <jjsoa1@gmail.com>
2020-02-07 15:24:47 -0800

    Initial project file for lint issue.

---
+++ javaLib/src/main/java/android/R.java
@@ -0,0 +1,13 @@
+package android;
+
+public class R {
+    private String a;
+
+    public R() {
+
+    }
+
+    public String getA() {
+        return a;
+    }
+}
---
+++ javaLib/src/main/java/com/example/javalib/MyClass.java
@@ -0,0 +1,15 @@
+package com.example.javalib;
+
+import android.R;
+
+public class MyClass {
+
+    public MyClass() {
+        assert false;
+
+        System.out.println("https://abc:user@password1" + R.class);
+        String store = "https://abc:user@password1";
+    }
+
+}
+
